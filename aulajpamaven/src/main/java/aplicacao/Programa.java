45bfd2633e86c7c1f3f05d7c96cc6f265dd52a47
Roberto Galbiatti F <robertogalbiatti@gmail.com>
2020-03-28 15:12:16 -0300

    Searching one person at DB, using one line of code with JPA

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -10,19 +10,15 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com"); //ID nulo para o próprio BD dar o ID
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		
-		//salvando os dados no banco de dados: EntityManager cria a conexão com o BD e o Factory instancia os objetos.
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); //isso vem do arquivo persistence.xml
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);//salva no BD
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
 		System.out.println("Pronto");
+		em.close();
+		emf.close();
 		
 	}
 
