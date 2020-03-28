f6e1d61e2fdc75c2d499a67258b48a5ee2cecc97
Roberto Galbiatti F <robertogalbiatti@gmail.com>
2020-03-28 15:20:53 -0300

    Showin' how to remove from DB, always when not just consult data, needs those 'transaction'

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -9,17 +9,19 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		
+
 		Pessoa p = em.find(Pessoa.class, 2);
-		
-		System.out.println(p);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+
 		System.out.println("Pronto");
 		em.close();
 		emf.close();
-		
+
 	}
 
 }
