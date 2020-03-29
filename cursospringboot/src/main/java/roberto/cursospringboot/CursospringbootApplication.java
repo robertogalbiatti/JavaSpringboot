99689e94f3f47c0b8b2a27a75b409bc77dadea32
Roberto Galbiatti F <robertogalbiatti@gmail.com>
2020-03-29 10:04:43 -0300

    First commit, inicialized

---
+++ cursospringboot/src/main/java/roberto/cursospringboot/CursospringbootApplication.java
@@ -0,0 +1,13 @@
+package roberto.cursospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringbootApplication.class, args);
+	}
+
+}
---
+++ cursospringboot/src/test/java/roberto/cursospringboot/CursospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package roberto.cursospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
