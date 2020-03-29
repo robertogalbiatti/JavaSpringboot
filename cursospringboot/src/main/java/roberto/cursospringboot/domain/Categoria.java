fc42ec8647bd6ad5ad2b965e6cc68e64c954ea1d
Roberto Galbiatti F <robertogalbiatti@gmail.com>
2020-03-29 12:43:37 -0300

    Added database h2 and automatically created

--- cursospringboot/src/main/java/roberto/cursospringboot/domain/Categoria.java
+++ cursospringboot/src/main/java/roberto/cursospringboot/domain/Categoria.java
@@ -2,9 +2,17 @@ package roberto.cursospringboot.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //gerando automaticamente estratégias dos Id's (chave primária)
 	private Integer id;
 	private String nome;
 	
