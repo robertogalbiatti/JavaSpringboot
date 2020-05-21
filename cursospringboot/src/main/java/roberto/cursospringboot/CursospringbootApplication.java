2140435936aeea95c5909f898c74400040c110a4
Roberto Galbiatti F <robertogalbiatti@gmail.com>
2020-05-21 16:21:25 -0300

    Creating Instanciation Operation of Instance

--- cursospringboot/src/main/java/roberto/cursospringboot/CursospringbootApplication.java
+++ cursospringboot/src/main/java/roberto/cursospringboot/CursospringbootApplication.java
@@ -1,14 +1,33 @@
 package roberto.cursospringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringbootApplication {
+import roberto.cursospringboot.domain.Categoria;
+import roberto.cursospringboot.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringbootApplication implements CommandLineRunner { // permite implementar um método auxiliar pra
+																		// executar uma ação quando a aplicação iniciar
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
 	}
 
+@Override
+public void run(String... args) throws Exception {
+	
+	Categoria cat1 = new Categoria(null, "Informática"); //lembrando que o banco cria automaticamente o número!
+	Categoria cat2 = new Categoria(null, "Escritório");
+	
+	categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	
 }
 
+}
